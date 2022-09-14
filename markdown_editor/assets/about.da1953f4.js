c22ac2be9cdb6fca49d4758ab8bbee38d1b18c3a
wxShawn <1806394760@qq.com>
2022-09-14 16:55:25 +0800

    更新

---
+++ markdown_editor/assets/about.da1953f4.js
@@ -0,0 +1,4 @@
+const t={title:"About",content:`# About
+**Project name**\uFF1AMarkdown Editor  
+**Auther**\uFF1AwxShawn  
+**Repository**\uFF1Ahttps://github.com/wxShawn/Markdown-Editor  `};export{t as default};
